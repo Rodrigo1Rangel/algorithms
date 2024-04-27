17e103aa27ba1117a4312d96ad86aa8b4908a054
Rodrigo Rangel <130096450+Rodrigo1Rangel@users.noreply.github.com>
2024-04-26 23:33:19 -0600

    group_of_anagram

---
+++ neetcode_roadmap/arrays_and_hashing/group_of_anagram.py
@@ -0,0 +1,65 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return
+the answer in any order. An Anagram is a word or phrase formed by rearranging
+the letters of a different word or phrase, typically using all the original
+letters exactly once.
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+
+
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+
+
+Constraints:
+    1 <= strs.length <= 104
+    0 <= strs[i].length <= 100
+    strs[i] consists of lowercase English letters.
+"""
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs):
+        result_hashmap = defaultdict(list) # default value is a list
+        # result_hashmap data type: <class 'collections.defaultdict'>
+
+        for word in strs:
+            # create a list of indexes that will be incremented
+            # as the letters appear in the following iteration.
+            letters_index = [0] * 26 # letters in the alphabet
+
+            for letter in word:
+                # 'ord("a")' is the baseline (0), as any other letter
+                # has a greater ord() value.
+                letters_index[ord(letter) - ord("a")] += 1 # increment at appearence
+
+            # here we can add key:value into the default
+            # to append the word in the hashmap value related to the letters_index (key)
+            result_hashmap[tuple(letters_index)].append(word) # 'append' works because default value is a list.
+            # tuple(), as a list can not be a key in a dictionary.
+        return result_hashmap.values()
+
+
+strs1 = ["eat","tea","tan","ate","nat","bat"]
+test1 = Solution().groupAnagrams(strs1)
+
+print()
+strs2 = [""]
+test2 = Solution().groupAnagrams(strs2)
+
+print()
+strs3 = ["a"]
+test3 = Solution().groupAnagrams(strs3)
---
+++ neetcode_roadmap/arrays_and_hashing/notes.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+"""
+Defaultdict is a sub-class of the dictionary class that returns a 
+dictionary-like object. The functionality of both dictionaries and
+defaultdict are almost same except for the fact that defaultdict
+never raises a KeyError. It provides a default value for the key
+that does not exists.
+defaultdict() has the first argument must be callable or None, which
+will be returned if the key is not found in the dictionary.
+"""
+
+a = dict()
+a[0] = (["1","2"])
+a[1] = (("1","2"))
+# ERROR: 'a[["1","2"]] = (["1","2"])'.
+a[("1","2")] = (("1","2"))
+
+# b = defaultdict(lambda: "There is no such key in the dictionary.")
+b = defaultdict(list)
+b[0] = (["1","2"])
+b[1] = (("1","2"))
+b[("1","2")] = (("1","2"))
+
+print(a)
+print(a[1])
+#print(a[100])
+#ERROR: 'a[key]' when the key does not exist in the dictionary
+print(b)
+print(b[1])
+print(b[100]) # defaultdict() returns an empty list when the key is not found
+
+# Multiplication of a list
+count = [0, 1, 2, 3] * 3
+print(count)
+
+# Unicode point for a one-character string
+# The unicode from 'a' to 'z' is linear, starting from 97
+unicode_point_a = ord('a')
+print(unicode_point_a)
+
+unicode_point_z = ord('z')
+print(unicode_point_z)
+print(unicode_point_z - unicode_point_a)
+
+print("\ndefault dict")
+default_dict = defaultdict(list)
+default_dict[1].append('2')
+default_dict[1].append('3')
+values_dict1 = default_dict.values()
+print(values_dict1)
